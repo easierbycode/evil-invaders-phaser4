4f02cf88bd855b5d79439c5a27db9e667c9f533d
▓▒░ ♔ Daniel ♔ ░▒▓ <563975+easierbycode@users.noreply.github.com>
2025-07-08 10:46:29 -0700

    Update vite.config.ts
    update repo name so Vite can link build files correctly

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 export default defineConfig(({ mode }) => ({
   base:
     mode === "production" && process.env.GITHUB_ACTIONS
-      ? "/phaser4-starter/" // ← replace with your repo name
+      ? "/evil-invaders-phaser4/" // ← replace with your repo name
       : "/",
   build: { outDir: "dist", emptyOutDir: true },
   resolve: { alias: { "@": resolve(__dirname, "src") } },
