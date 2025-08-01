2ec281fa934c22639161d55bc078a5d783d5b7c2
EasierByCode <daniel@easierbycode.com>
2025-08-01 16:11:08 -0700

    360x640

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // Constants
 // export const WIDTH = 1680;
 // export const HEIGHT = 1050;
-// export const WIDTH = 360;
-export const WIDTH = 400;
+export const WIDTH = 360;
+// export const WIDTH = 400;
 export const HEIGHT = 640;
 export const ZOOM_MIN = 1.0;
 export const ZOOM_MAX = 2;
