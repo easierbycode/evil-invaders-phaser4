d0492ca2e251098c601ec1b12a29ce57a1107a8f
EasierByCode <daniel@easierbycode.com>
2025-09-25 22:50:41 -0700

    fix mutoid head explosion freezing game

--- src/scenes/MutoidScene.ts
+++ src/scenes/MutoidScene.ts
@@ -52,7 +52,8 @@ export default class MutoidScene extends Phaser.Scene {
   private armRight!: Phaser.GameObjects.Image;
   private torsoLeft!: Phaser.GameObjects.Image;
   private torsoRight!: Phaser.GameObjects.Image;
-  private head!: Phaser.GameObjects.Sprite;
+  private head: Phaser.GameObjects.Sprite | null = null;
+  private mutoidFloatTween: Phaser.Tweens.Tween | null = null;
 
   /* START-USER-CODE */
   // Write your code here
@@ -71,7 +72,8 @@ export default class MutoidScene extends Phaser.Scene {
     const treadRight = this.add.sprite(0, 0, "mutoid-tank-tread", TREAD_FRAME).setOrigin(0, 1).setFlipX(true);
     const treadFrontLeft = this.add.sprite(0, 0, "mutoid-tank-tread-front", TREAD_FRAME).setOrigin(0, 0);
     const treadFrontRight = this.add.sprite(0, 0, "mutoid-tank-tread-front", TREAD_FRAME).setOrigin(1, 0).setFlipX(true);
-    this.head = this.add.sprite(0, 0, "mutoid-head", HEAD_FRAME).setOrigin(0.5, 0);
+    const head = this.add.sprite(0, 0, "mutoid-head", HEAD_FRAME).setOrigin(0.5, 0);
+    this.head = head;
     this.armLeft = this.add.image(0, 0, "mutoid-arm").setOrigin(1, 0);
     this.armRight = this.add.image(0, 0, "mutoid-arm").setOrigin(1, 0).setFlipX(true);
 
@@ -94,7 +96,7 @@ export default class MutoidScene extends Phaser.Scene {
       this.torsoRight,
       this.armLeft,
       this.armRight,
-      this.head,
+      head,
     ]);
 
     this.ensureAnimation("mutoid-tank-tread-spin", "mutoid-tank-tread");
@@ -128,7 +130,7 @@ export default class MutoidScene extends Phaser.Scene {
     this.armLeft.setPosition(-torsoWidth + 15, 2);
     this.armRight.setPosition(armWidth + torsoWidth - 15, 2);
 
-    this.head.setPosition(0, -HEAD_OFFSET_FROM_TORSO_TOP);
+    head.setPosition(0, -HEAD_OFFSET_FROM_TORSO_TOP);
 
     const sprites = [
       this.torsoLeft,
@@ -141,7 +143,7 @@ export default class MutoidScene extends Phaser.Scene {
       treadFrontRight,
       this.armLeft,
       this.armRight,
-      this.head,
+      head,
     ];
 
     const leftmost = Math.min(
@@ -160,8 +162,8 @@ export default class MutoidScene extends Phaser.Scene {
     this.mutoidContainer.setSize(rightmost - leftmost, bottommost - topmost);
 
     this.mutoidParts = this.physics.add.group();
-    this.physics.world.enable([this.armLeft, this.armRight, this.torsoLeft, this.torsoRight, this.head]);
-    this.mutoidParts.addMultiple([this.armLeft, this.armRight, this.torsoLeft, this.torsoRight, this.head]);
+    this.physics.world.enable([this.armLeft, this.armRight, this.torsoLeft, this.torsoRight, head]);
+    this.mutoidParts.addMultiple([this.armLeft, this.armRight, this.torsoLeft, this.torsoRight, head]);
 
     this.mutoidParts.getChildren().forEach(part => {
         (part.body as Phaser.Physics.Arcade.Body).setImmovable(true);
@@ -180,9 +182,9 @@ export default class MutoidScene extends Phaser.Scene {
       : ["atlas_s1", "atlas_s2", "atlas_s3", "atlas_s3"];
     this.ensureExplicitAnimation("mutoid-head-back", "mutoid-head", headBackFrames, 2, 0);
 
-    this.head.setFrame(HEAD_FRAME);
+    head.setFrame(HEAD_FRAME);
 
-    this.animateMutoid(this.head);
+    this.animateMutoid();
 
     // @ts-ignore
     window.gameScene = this;
@@ -240,7 +242,7 @@ export default class MutoidScene extends Phaser.Scene {
     }
   }
 
-  private animateMutoid(head: Phaser.GameObjects.Sprite) {
+  private animateMutoid() {
 
     const ease = this.secondLoop ? "Elastic.easeInOut" : "Quad.easeInOut";
 
@@ -248,6 +250,8 @@ export default class MutoidScene extends Phaser.Scene {
     // to look forward when going down, and look back when going up.
     // Pause at the top and bottom of the movement.
     // Repeat forever.
+
+    this.stopMutoidFloatTween();
 
     const tween = this.tweens.add({
       targets: this.mutoidContainer,
@@ -261,30 +265,49 @@ export default class MutoidScene extends Phaser.Scene {
       // ease: "Quad.easeInOut",
       // ease: "Elastic.easeInOut",
       ease,
-      onStart: () => this.playHeadForward(head),
-      onYoyo: () => this.playHeadBackward(head),
-      onRepeat: () => this.playHeadForward(head),
-      onComplete: () => this.playHeadIdle(head),
-      onStop: () => this.playHeadIdle(head),
+      onStart: () => this.playHeadForward(),
+      onYoyo: () => this.playHeadBackward(),
+      onRepeat: () => this.playHeadForward(),
+      onComplete: () => this.playHeadIdle(),
+      onStop: () => this.playHeadIdle(),
     });
 
-    this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => tween.stop());
+    this.mutoidFloatTween = tween;
+
+    this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => this.stopMutoidFloatTween());
   }
 
-  private playHeadIdle(head: Phaser.GameObjects.Sprite) {
+  private stopMutoidFloatTween() {
+    if (!this.mutoidFloatTween) return;
+
+    this.mutoidFloatTween.stop();
+    this.mutoidFloatTween.remove();
+    this.mutoidFloatTween = null;
+  }
+
+  private playHeadIdle() {
+    const head = this.head;
+    if (!head || !head.scene) return;
+
     head.stop();
     head.setFrame(HEAD_FRAME);
   }
 
-  private playHeadForward(head: Phaser.GameObjects.Sprite) {
+  private playHeadForward() {
+    const head = this.head;
+    if (!head || !head.scene) return;
+
     head.play({ key: "mutoid-head-forward", repeat: -1, frameRate: 5 });
   }
 
-  private playHeadBackward(head: Phaser.GameObjects.Sprite) {
+  private playHeadBackward() {
+    const head = this.head;
+    if (!head || !head.scene) return;
+
     head.play({ key: "mutoid-head-back", repeat: 0, frameRate: 2 });
     head.once(
       Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + "mutoid-head-back",
-      () => this.playHeadIdle(head)
+      () => this.playHeadIdle()
     );
   }
 
@@ -326,15 +349,22 @@ export default class MutoidScene extends Phaser.Scene {
     }
     // Target: Head
     else {
-      if (part === this.head) {
+      if (this.head && part === this.head) {
           this.mutoidHeadHealth -= damageDealt;
-          if (this.mutoidHeadHealth <= 0) {
-              const headWorldPos = this.head.getWorldTransformMatrix();
+          if (this.mutoidHeadHealth <= 0 && this.head) {
+              const head = this.head;
+              const headWorldPos = head.getWorldTransformMatrix();
               const explosionX = headWorldPos.tx;
               const explosionY = headWorldPos.ty;
 
-              this.head.destroy();
-              this.mutoidContainer.destroy(); // Or trigger a bigger explosion
+              this.stopMutoidFloatTween();
+
+              head.destroy();
+              this.head = null;
+
+              if (this.mutoidContainer.active) {
+                  this.mutoidContainer.destroy(); // Or trigger a bigger explosion
+              }
 
               const explosionGroup = this.add.group();
               for (let i = 0; i < 50; i++) {
